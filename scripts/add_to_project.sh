a4ea6b5ef62213f8a981ac8ebf320db1b4d728be
Zane Hitchcox <zwhitchcox@gmail.com>
2020-10-31 15:32:24 -0400

    script

--- scripts/add_to_project.sh
+++ scripts/add_to_project.sh
@@ -9,3 +9,10 @@ mkdir -p .vscode
 pushd .vscode
 wget https://raw.githubusercontent.com/zwhitchcox/express-proxy-react/main/server/tsconfig.json
 popd
+echo "add the following scripts to your package.json"
+echo ""
+echo  '"scripts": {
+    "start-cra": "react-scripts start",
+    "start-express": "ts-node --project server/tsconfig.json server/index.ts",
+    "parallel": "npm-run-all --parallel start-react start-express",
+...'
